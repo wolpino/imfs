f37dd7af250134cf45c2a80a584139e12182a4aa
wolpino <ariwolpin@gmail.com>
2025-09-07 11:19:44 -0700

    add read, write with tests

--- file_system/file_system/file_system.py
+++ file_system/file_system/file_system.py
@@ -1,3 +1,4 @@
+import argparse
 import cmd
 from model import Node
 
@@ -21,23 +22,56 @@ class FileSystem(cmd.Cmd):
         "List files and directories in the current directory."
         print(self.current_directory.children.keys())
 
-    def do_create(self, name: str, file: bool=False) -> None:
-        "Create a file or directory in the current directory. Usage: create <name> [--file]"
+    def do_create_dir(self, name: str) -> None:
+        #TODO combine creates and add file flag
+        "Create a file or directory in the current directory. Usage: create_file <name>"
         newNode = Node()
         newNode.name = name
-        newNode.isFile = file
+        newNode.isFile = False
         self.current_directory.children[name] = newNode
-        print(f"{'File' if file else 'Directory'} created")
+        print(f"Directory created")
+
+    def do_create_file(self, name: str) -> None:
+        "Create a file or directory in the current directory. Usage: create_file <name>"
+        newNode = Node()
+        newNode.name = name
+        newNode.isFile = args.file
+        self.current_directory.children[name] = newNode
+        print(f"File created")
+
+    def do_write(self, argline:str):
+        "Write content to a file. Usage: write <file_name> <content>"
+        parts = argline.split(' ', 1)
+        if len(parts) != 2:
+            print("Usage: write <file_name> <content>")
+            print("Ex: write myfile 'test content'")
+
+            return
+        file, content = parts
+        fileNode = self.current_directory.children[file]
+        fileNode.content = content
+        print(f"Content written to {file}")
 
     def do_read(self, file: str):
         "Read the content of a file. Usage: read <file_name>"
         fileNode = self.current_directory.children[file]
         print(fileNode.content) # TODO enhance output
 
+  
     def do_exit(self, argline:str) -> bool:
         """Exit the file system."""
         print("Exiting the file system.")
         return True
     
+# Set up argument parser
+parser = argparse.ArgumentParser(description='File renaming tool.')
+parser.add_argument('-f', '--file', help='Create a file.', default=False, action='store_true')
+parser.add_argument('-r', '--replace', nargs=2, help='Replace a substring in each file name. Usage: -r old_string new_string')
+
+args = parser.parse_args()
+
+    
 if __name__ == "__main__":
     FileSystem().cmdloop()
+
+    
--- file_system/tests/test_file_system.py
+++ file_system/tests/test_file_system.py
@@ -20,9 +20,9 @@ def test_do_list(capsys):
     output = capsys.readouterr()
     assert output.out == expected_list
 
-def test_do_create(capsys):
+def test_do_create_dir(capsys):
     fs = FileSystem()
-    fs.do_create(name="testDir")
+    fs.do_create_dir(name="testDir")
     output = capsys.readouterr()
     assert output.out == "Directory created\n"
 
@@ -30,11 +30,37 @@ def test_do_create(capsys):
     output = capsys.readouterr()
     assert output.out == "dict_keys(['testDir'])\n"
     
-    fs.do_create(name="testFile", file=True)
+    # fs.do_create(name="testFile", file=True)
+    # output = capsys.readouterr()
+    # assert output.out == "File created\n"
+    
+    # fs.do_list()
+    # output = capsys.readouterr()
+    # assert output.out == "dict_keys(['testDir', 'testFile'])\n"
+
+def test_do_create_file(capsys):
+    #TODO check object for isFile not just print output
+    fs = FileSystem()
+    fs.do_create_file(name="testFile")
     output = capsys.readouterr()
     assert output.out == "File created\n"
-    
-    fs.do_list()
-    output = capsys.readouterr()
-    assert output.out == "dict_keys(['testDir', 'testFile'])\n"
 
+def test_do_write(capsys):
+    fs = FileSystem()
+    file ="testFile"
+    fs.do_create_file(name=file)
+    capsys.readouterr() # clear output
+    fs.do_write("testFile This is test content")
+    output = capsys.readouterr()
+    assert output.out == (f"Content written to {file}\n")
+
+def do_read(capsys):
+    fs = FileSystem()
+    file ="testFile"
+    fs.do_create_file(name=file)
+    capsys.readouterr() # clear output
+    fs.do_write("testFile This is test content")
+    capsys.readouterr() # clear output      
+    fs.do_read(file)
+    output = capsys.readouterr()
+    assert output.out == "This is test content\n"
